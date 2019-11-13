eb5209189481998f4f16e38830ae6b0ab4fe45a1
Anja Laube <mail@anjalaube.com>
2019-11-13 12:43:36 +0100

    Rails new with frontend and form gems

---
+++ app/models/restaurant.rb
@@ -0,0 +1,5 @@
+class Restaurant < ApplicationRecord
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :stars, inclusion: { in: [1, 2, 3, 4, 5] }
+end
